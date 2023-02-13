e79e87ba02efcb77fe8bc6dd69c62f04e6d59e5e
Nadiah <nadiahmohamadm@gmail.com>
2023-02-13 11:50:47 +0800

    Use env for sensitive info

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 //jshint esversion: 6
+require("dotenv").config()
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
-// const https = require("https");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -37,23 +37,19 @@ app.post("/", function (req, res) {
     method: "POST",
     url: "https://us21.api.mailchimp.com/3.0/lists/2028ff4f7e/members",
     headers: {
-      Authorization:
-        "Basic TmFkaWFoTToxMWM4YTM4ZmNmZTViMzY3MTVmYTUwODhkZDVkYTExOS11czIx",
+      Authorization: process.env.MAILCHIMP_AUTH,
       "Content-Type": "application/json",
-      // Cookie: 'ak_bmsc=F9676713F72BD24CB41F35CB1E1ED090~000000000000000000000000000000~YAAQf1YbOiX05C+GAQAA5jilNxLqKtEY5CZHxugsU8l+esB4hfcNB3NqPG47r8RXslix+TVspDZCWH4YIG0mFBNBKm9Qz91uO37RdaRZvyxatmg84MyDuMBHcUbrbHX6GSpvhXzRacmSA7FQsEpEpQkuJnKeEQa2//bUde/B7ivb5Uqivo74VG+b8L5J/YIOXN35hKlCXW2kn0ACBA3B0Ql3gUGatsMDeoFfXIyHuPKIYAHvQzAGqzXac+5jrIkIpX0DrDRpj7PpkNZCfl1DbNKF6t/m3/68C65CDST6Txrb5LfMe6pUpbwyf03XgMTJKwerdZKXPtH1P5YiPhg4yxYBZUvbs+WL6P9MiwZx4CLa2LR5ZiLTCErGr4sOjcmC8Wk=; bm_sv=A8A18701E063D8136AB6FD0034F45B67~YAAQf1YbOvP15C+GAQAAbA6qNxKyuMDMZHA3y0cXcPX1Xsl/ZRTtRYxOT2nCay5HvMbn/CTPp7SFDv89edo9Hj3Lwwmanoi9gkfqn7XvHyq+Wmp2YnsU3aIiNu8ROPYKjQn7d0qJmRroyVnsWPyBnsPyuYqNtd9Iu6EYZwC1o8lTHGVATeTeuF0kwjGEXfx/3NRfsJuPRr6w5Yosk+KmnXTtl43dnbSjwh9K+mA2oYrf8eyQS+uwUYDl8w4FP9WnUbbslL3rXA==~1'
     },
     body: jsonData,
   };
 
   request(options, function (error, response) {
-    // if (error) throw new Error(error);
+    if (error) throw new Error(error);
     console.log(response.body);
     if (response.statusCode === 200) {
       res.sendFile(`${__dirname}/success.html`);
-      // res.send("successfully subscribed!")
     } else {
       res.sendFile(`${__dirname}/failure.html`);
-      // res.send("something went wrong!")
     }
   });
 });
@@ -65,9 +61,3 @@ app.post("/failure", function (req, res) {
 app.listen(port, function () {
   console.log(`The server is running on port ${port}`);
 });
-
-//API key
-//11c8a38fcfe5b36715fa5088dd5da119-us21
-
-//List ID or Audience ID
-//2028ff4f7e
